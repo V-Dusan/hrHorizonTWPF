e209f52f35dcc5c030b6f333437910e2cdb3dfc0
V-Dusan <vasilijevic.dusan@gmail.com>
2020-12-28 18:35:19 +0100

    add- new test window

--- hrHorizonT.UI/ShellWindow.xaml.cs
+++ hrHorizonT.UI/ShellWindow.xaml.cs
@@ -19,17 +19,25 @@ namespace hrHorizonT.UI
     public partial class ShellWindow : Window
     {
         private MainViewModel _viewModel;
+        private MainViewModel _viewModel1;
 
-        public ShellWindow(MainViewModel viewModel)
+        public ShellWindow(MainViewModel viewModel, MainViewModel viewModel1)
         {
             InitializeComponent();
-            this._viewModel = viewModel; 
+            this._viewModel = viewModel;
+            this._viewModel1 = viewModel1;
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             MainWindow newMainWindow = new MainWindow(_viewModel);
             newMainWindow.Show();
+        }
+
+        private void MenuItem_Test(object sender, RoutedEventArgs e)
+        {
+            TestWindow newTestWindow = new TestWindow(_viewModel1);
+            newTestWindow.Show();
         }
     }
 }
