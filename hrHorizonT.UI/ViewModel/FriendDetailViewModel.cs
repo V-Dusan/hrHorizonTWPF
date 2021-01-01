b09087b9cf6a25e3c49f3bc27a386234d8a61796
V-Dusan <vasilijevic.dusan@gmail.com>
2021-01-01 19:44:22 +0100

    add-When another user deleted an entity

--- hrHorizonT.UI/ViewModel/FriendDetailViewModel.cs
+++ hrHorizonT.UI/ViewModel/FriendDetailViewModel.cs
@@ -156,6 +156,14 @@ namespace hrHorizonT.UI.ViewModel
             }
             catch (DbUpdateConcurrencyException ex)
             {
+                var databaseValues = ex.Entries.Single().GetDatabaseValues();
+                if (databaseValues == null)
+                {
+                    MessageDialogService.ShowInfoDialog("The entity has been deleted ny another user");
+                    RaiseDetailDeletedEvent(Id);
+                    return;
+                }
+
                 var result = MessageDialogService.ShowOkCancelDialog("The entity has been changed in the meantime by someone else." +
                     " Click OK to save your changes anyway, click Cancel to reload the entity from the database.", "Question");
 
